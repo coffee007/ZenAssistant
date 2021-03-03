40c326510f5275d1e475adac2112f5b67c9ce123
Aryaman <59313789+Aryaman-Das@users.noreply.github.com>
2021-03-03 17:16:01 +0530

    c

--- chatbot.py
+++
@@ -1,7 +0,0 @@
-from dadjoke import dadjoke
-
-def chatbot(text):
-    if "tell" in text and "joke" in text:
-        return dadjoke()
-    else:
-        return "Some response"
---
+++ features.py
@@ -0,0 +1,10 @@
+from dadjoke import dadjoke
+
+
+class Assistant():
+    def __init__(self, name):
+        self.name = name
+    def chatbot(self, text):
+        return 'some response'
+    def dadjoke(self):
+        dadjoke()
