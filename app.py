7da337d477d3d0d4b308f44f70d304acec7ae9e4
Aryaman <59313789+Aryaman-Das@users.noreply.github.com>
2021-03-03 20:34:17 +0530

    HeyItsMe - 20:32 IST (UTC+5:30) changed folder structure for organization added requirements.txt added a text_summarizer.py updated app.py and features.py

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from Features import Assistant
+from features import Assistant
 
 bot = Assistant("Zen")
 
 joke = ["joke", "laugh", "funny"]
+name = ["name", "who are you"]
+google = ["google", "search"]
+summary = ["summary"]
 
 while True:
-    text = input("Enter message: ")
-    for word in joke:
-        if word in text:
-            print(bot.dadjoke())
+    text = input("Enter message: ").lower()
+    if any(word in joke for word in text.split()):
+        print(bot.dadjoke())
+    elif any(word in name for word in text.split()):
+        print("My name is {}".format(bot.name))
+    elif any(word in google for word in text.split()):
+        print(bot.GoogleSearch(text))
+    elif any(word in summary for word in text.split()):
+        print(bot.summarize_text(text))
