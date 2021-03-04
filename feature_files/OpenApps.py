3a9bfc10ae832b2db4996e2f9864964289959da0
Jaishnu <73887147+Jaishnu@users.noreply.github.com>
2021-03-04 10:44:22 +0530

    INFI just made the functio ignore the case sensitivity
    Signed-off-by: Jaishnu <73887147+Jaishnu@users.noreply.github.com>

--- feature_files/OpenApps.py
+++ feature_files/OpenApps.py
@@ -6,7 +6,7 @@ def find(name):
     # searches the folders
     for root, dirs, files in os.walk("C:\\Users\\raju srinivasan\\Desktop"):
         # if it finds it, it prints the path and returns it
-        if str(name + ".lnk") in files:
+        if str(name.lower() + ".lnk") in str(files).lower():
             path = str((root + "\\" + name + ".lnk"))
             print(path)
         # else it does this
@@ -22,4 +22,4 @@ def OpenAPP(name):
     O = find(str(name))
     os.startfile(str(O))
 
-OpenAPP("Spotify")
+OpenAPP("tlauncher")
