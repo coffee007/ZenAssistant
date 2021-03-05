a0d5b97e69d6a83ec745d32b02ffc10d4c8f6782
Aryaman <59313789+Aryaman-Das@users.noreply.github.com>
2021-03-05 21:03:20 +0530

    MAJOR update. NLP part is complete for v1.0 and added 2 files - intents.json and commands_and_intents.json. The intents.json is for user inputs and their corresponding responses, IF no command is detected. commands_and_intents.json is for user inputs and the corresponding functions. Everyone can edit the commands_and_intents.json and intents.json files and add more possible patterns in the user's input. For example, you can add "make me laugh" for the patterns in the "jokes" function.
    Added a parameter called "reqs_confirm" which asks users for inputs to the program. For example, if user says "search google" then the program asks the user what should be searched.
    
    Also fixed some errors in Dictionary.py
    
    Also fixed some errors in
    
    Signed-off-by: Aryaman <59313789+Aryaman-Das@users.noreply.github.com>

--- feature_files/natural_language.py
+++ feature_files/natural_language.py
@@ -1,6 +1,35 @@
 from fuzzywuzzy import fuzz
-import nltk
 import json
+import random
 
-file = open("data/intents.json", 'r+')
-intents = json.load(file)
+file = open("feature_files/data/intents.json", 'r+')
+file2 = open("feature_files/data/commands_and_intents.json", "r+")
+intents = list(dict(json.load(file)).get("intents"))
+commands = list(dict(json.load(file2)).get("commands"))
+
+
+def talk(text, chat_history = ''):
+    text = text.lower()
+    max_match = {"intent": "Sorry, I didn't get that.", "match": 0}
+    for command in commands:
+        command = dict(command)
+        for j in list(command.get("patterns")):
+            n = fuzz.partial_ratio(text, j)
+            if n > max_match.get("match"):
+                max_match["match"] = n
+                max_match["intent"] = list(command.get("commands"))[0]
+    if max_match["match"] > 80:
+        return [max_match.get("intent"), "function"]
+    max_match = {"intent": "Sorry, I didn't get that.", "match": 0}
+    for intent in intents:
+        intent = dict(intent)
+        for i in list(intent.get("patterns")):
+            m = fuzz.partial_ratio(text, i)
+            if m > max_match.get("match"):
+                max_match["match"] = m
+                max_match["intent"] = random.choice(list(intent.get("responses")))
+
+    return [max_match.get("intent"), "response"]
+
+
+
--- feature_files/text_summarizer.py
+++ feature_files/text_summarizer.py
@@ -8,74 +8,77 @@ import networkx as nx
 
 
 def summary(text):
-    def read_article(text):
-        article = text.split(". ")
-        sentences = []
+    try:
+        def read_article(text):
+            article = text.split(". ")
+            sentences = []
 
-        for sentence in article:
-            print(sentence)
-            sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
-        sentences.pop()
+            for sentence in article:
+                print(sentence)
+                sentences.append(sentence.replace("[^a-zA-Z]", " ").split(" "))
+            sentences.pop()
 
-        return sentences
+            return sentences
 
-    def sentence_similarity(sent1, sent2, stopwords=None):
-        if stopwords is None:
-            stopwords = []
+        def sentence_similarity(sent1, sent2, stopwords=None):
+            if stopwords is None:
+                stopwords = []
 
-        sent1 = [w.lower() for w in sent1]
-        sent2 = [w.lower() for w in sent2]
+            sent1 = [w.lower() for w in sent1]
+            sent2 = [w.lower() for w in sent2]
 
-        all_words = list(set(sent1 + sent2))
+            all_words = list(set(sent1 + sent2))
 
-        vector1 = [0] * len(all_words)
-        vector2 = [0] * len(all_words)
+            vector1 = [0] * len(all_words)
+            vector2 = [0] * len(all_words)
 
-        # build the vector for the first sentence
-        for w in sent1:
-            if w in stopwords:
-                continue
-            vector1[all_words.index(w)] += 1
-
-        # build the vector for the second sentence
-        for w in sent2:
-            if w in stopwords:
-                continue
-            vector2[all_words.index(w)] += 1
-
-        return 1 - cosine_distance(vector1, vector2)
-
-    def build_similarity_matrix(sentences, stop_words):
-        # Create an empty similarity matrix
-        similarity_matrix = np.zeros((len(sentences), len(sentences)))
-
-        for idx1 in range(len(sentences)):
-            for idx2 in range(len(sentences)):
-                if idx1 == idx2:  # ignore if both are same sentences
+            # build the vector for the first sentence
+            for w in sent1:
+                if w in stopwords:
                     continue
-                similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
+                vector1[all_words.index(w)] += 1
 
-        return similarity_matrix
+            # build the vector for the second sentence
+            for w in sent2:
+                if w in stopwords:
+                    continue
+                vector2[all_words.index(w)] += 1
 
-    def generate_summary(file_name, top_n=5):
-        stop_words = stopwords.words('english')
-        summarize_text = []
+            return 1 - cosine_distance(vector1, vector2)
 
-        # Step 1 - Read text anc split it
-        sentences = read_article(file_name)
+        def build_similarity_matrix(sentences, stop_words):
+            # Create an empty similarity matrix
+            similarity_matrix = np.zeros((len(sentences), len(sentences)))
 
-        # Step 2 - Generate Similary Martix across sentences
-        sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
+            for idx1 in range(len(sentences)):
+                for idx2 in range(len(sentences)):
+                    if idx1 == idx2:  # ignore if both are same sentences
+                        continue
+                    similarity_matrix[idx1][idx2] = sentence_similarity(sentences[idx1], sentences[idx2], stop_words)
 
-        sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
-        scores = nx.pagerank_numpy(sentence_similarity_graph)
+            return similarity_matrix
 
-        # Step 4 - Sort the rank and pick top sentences
-        ranked_sentence = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+        def generate_summary(file_name, top_n=5):
+            stop_words = stopwords.words('english')
+            summarize_text = []
 
-        for i in range(top_n):
-            summarize_text.append(" ".join(ranked_sentence[i][1]))
+            # Step 1 - Read text anc split it
+            sentences = read_article(file_name)
 
-        return "Summarized Text: \n", ". ".join(summarize_text)
+            # Step 2 - Generate Similary Martix across sentences
+            sentence_similarity_martix = build_similarity_matrix(sentences, stop_words)
 
-    return generate_summary(text)
+            sentence_similarity_graph = nx.from_numpy_array(sentence_similarity_martix)
+            scores = nx.pagerank_numpy(sentence_similarity_graph)
+
+            # Step 4 - Sort the rank and pick top sentences
+            ranked_sentence = sorted(((scores[i], s) for i, s in enumerate(sentences)), reverse=True)
+
+            for i in range(top_n):
+                summarize_text.append(" ".join(ranked_sentence[i][1]))
+
+            return "Summarized Text: \n", ". ".join(summarize_text)
+
+        return generate_summary(text)
+    except:
+        return "Error, please ensure your paragraph has more than 5 sentences."
