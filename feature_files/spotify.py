52e374d524cf7fa630fcfc9e6676a79efa7ac628
ldvlpr-dev <ldvlpr.dev@gmail.com>
2021-03-05 18:35:49 +0100

    Spotify feature can change playing track

--- feature_files/spotify.py
+++ feature_files/spotify.py
@@ -5,7 +5,7 @@ from spotipy.oauth2 import SpotifyClientCredentials, SpotifyOAuth
 class SpotifyFeature:
     is_spotify_access_ready = False
 
-    def __init__(self, api_credentials_file, log_user_in=False):
+    def __init__(self, api_credentials_file, log_user_in=False, redirect_uri=""):
         with open(api_credentials_file, "r") as file:
             for i, line in enumerate(file.readlines()):
                 if i == 0:
@@ -17,8 +17,8 @@ class SpotifyFeature:
                 auth_manager=SpotifyOAuth(
                     client_id=self.client_id,
                     client_secret=self.client_secret,
-                    redirect_uri=self.redirect_uri,
-                    scope="user-read-playback-state user-modify-playback-state user-read-currently-playing"
+                    redirect_uri=redirect_uri,
+                    scope="user-read-playback-state,user-modify-playback-state,user-read-currently-playing"
                 )
             )
             self.is_spotify_access_ready = True
@@ -32,17 +32,24 @@ class SpotifyFeature:
             )
             self.is_spotify_access_ready = True
 
-    def search_music(self, query):
-        results = self.spotify_access.search(q=query, limit=20)
+    def search_music(self, query, nb_results=20):
+        results = self.spotify_access.search(q=query, limit=nb_results)
         return "\n".join(
             [str(index) + " - " + track["name"]
                 for index, track in enumerate(results["tracks"]["items"])]
         )
 
+    def play_music(self, uris: list):
+        devices = self.spotify_access.devices()["devices"]
+        if len(devices) == 0:
+            return {"Error": "No device running Spotify"}
+        else:
+            self.spotify_access.start_playback(uris=uris)
+
 
 def test_feature(api_keys_path):
-    spotify = SpotifyFeature(api_keys_path)
-    print(spotify.search_music("blinding lights"))
+    spotify = SpotifyFeature(api_keys_path, True, "http://localhost:4321")
+    spotify.play_music([input("track uri: ")])
 
 
 if __name__ == "__main__":
