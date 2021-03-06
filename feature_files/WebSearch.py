06d1e340663943c113dcef1c3c3879f2e41abb9c
Jaishnu <73887147+Jaishnu@users.noreply.github.com>
2021-03-06 13:32:54 +0530

    made WebSearch.py(with pywhatkit)

--- feature_files/WebSearch.py
+++ feature_files/WebSearch.py
@@ -1,26 +1,7 @@
-from selenium import webdriver
-from selenium.webdriver.common.keys import Keys
-from selenium.webdriver.chrome.options import Options
-
-chrome_options = Options()
-chrome_options.add_argument("--window-size=1024x768")
-chrome_options.add_argument("--headless")
-driver = webdriver.Chrome(chrome_options=chrome_options)
+import pywhatkit
 
 
-def ask_google(query):
-    # Search for query
-    query = query.replace(' ', '+')
-
-    driver.get('http://www.google.com/search?q=' + query)
-
-    # Get text from Google answer box
-
-    answer = driver.execute_script(
-        "return document.elementFromPoint(arguments[0], arguments[1]);",
-        350, 230).text
-
-    return answer
+def googleSearch(query):
+    return pywhatkit.search(query)
 
 
-ask_google("what is the time")
