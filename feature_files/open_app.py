ddb8f5bcb886832e6161cf5d7abc6fbea4d887cc
Jaishnu <73887147+Jaishnu@users.noreply.github.com>
2021-03-15 20:21:27 +0530

    made open apps

--- feature_files/open_app.py
+++ feature_files/open_app.py
@@ -1,40 +1,40 @@
 import os
-
-file_added = False
-try:
-    if not file_added:
-        directory = os.path.join("userapps")
-        os.mkdir(directory)
-        print("File added")
-        file_added = True
-    else:
-        x = "File added already"
-        print(x)
-except OSError as e:
-    print("Folder created:", e)
+import re
+import win32api
 
 
-# find the file that needs to be opened
-def find(name):
-    # searches the folders
-    for root, dirs, files in os.walk(directory):
-        # if it finds it, it prints the path and returns it
-        if str(name.lower() + ".lnk") in str(files).lower():
-            path = str((root + "\\" + name + ".lnk"))
-            print(path)
-            # else it does this
-        else:
-            error = (
-                "File not found , make sure the shortcut is added to " + directory)
-            print(error)
-            path = None
 
-        return path
+def find_file(root_folder, rex):
+    found = False
+    for root, dirs, files in os.walk(root_folder):
+        for f in files:
+            result = rex.search(f)
+            if not found:
+                output = "file not found"
+                if result:
+                    print(str(os.path.join(root, f)))
+                    return os.path.join(root, f)
+                    found = True
 
 
-def OpenApp(name):
-    app_to_be_opened = find(str(name))
+def find_file_in_all_drives(file_name):
+    # create a regular expression for the file
+    rex = re.compile(file_name + '.exe')
+    print(file_name + '.exe')
+    for drive in win32api.GetLogicalDriveStrings().split('\000')[:-1]:
+        a = find_file(drive, rex)
+    return a
+
+
+def openApp(app_name):
     try:
-        os.startfile(str(app_to_be_opened))
-    except Exception as e:
-        print("")
+        os.startfile(str(find_file_in_all_drives(str(app_name))))
+        result = "succesfull"
+        print (result)
+    except OSError as e:
+        result = "failed,cannot find the app on ur device"
+        print(e,result)
+        return result
+
+if __name__ == "__main__":
+    openApp("Spotify") # <- app name goes here, this is an example
